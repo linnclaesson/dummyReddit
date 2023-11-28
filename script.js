a28f1b5f3d37195f60502af735e5699b9bbfc779
linnclaesson <linn.denise.claesson@gmail.com>
2023-11-28 13:44:29 +0100

    render fetched posts in browser

--- script.js
+++ script.js
@@ -7,15 +7,27 @@ fetchDummy = () => {
     fetch('https://dummyjson.com/posts?limit=4&skip=20')
         .then((res) => res.json())
         .then((res) => {
-            console.log('fetched posts');
+            console.log('fetch successful');
             // call renderDummyPosts
             renderDummyPosts(res.posts);
         });
 };
 
-// render dummy posts
+// render dummy posts: create elements to display the fetched data in browser
 renderDummyPosts = (res) => {
+    // log the fetched data
     console.log(res);
+
+    // loop through fetched data
+    for (let i = 0; i < res.length; i++) {
+        let item = res[i];
+        // create elements
+        let post = document.createElement('div');
+        // assign data to element
+        post.innerText = item.title;
+        // append created elements
+        postsContainer.append(post);
+    }
 };
 
 // CALL FUNCTIONS
